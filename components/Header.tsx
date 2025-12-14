59be4e43668624381f07e6d8f38e1ef854aca689
jeancharlescano <jeancharles.cano@outlook.fr>
2025-12-14 18:31:47 +0100

    ajout du logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
 import React from "react";
 
 const Header = React.memo(
   ({ onSearch }: { onSearch: (query: string) => void }) => {
     return (
       <header className="h-16 border-b border-white rounded-b flex items-center justify-center lg:justify-start lg:px-4 shadow-white shadow-sm px-4">
-        <h1 className="hidden lg:block font-bold lg:w-1/3">Dechno</h1>
+        <div className="hidden  lg:w-1/3 lg:flex lg:items-center">
+          <Image src="/dechno.png" alt="Dechno logo" width={60} height={60} />
+        </div>
 
         <input
           id="search"
